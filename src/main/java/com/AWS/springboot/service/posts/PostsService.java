4fd9551639c4f27833f90623680726f819b8b343
moongi <pearlmoon997@gmail.com>
2022-03-07 16:00:53 +0900

    View Templates / posts save&select

--- src/main/java/com/AWS/springboot/service/posts/PostsService.java
+++ src/main/java/com/AWS/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.AWS.springboot.service.posts;
 
 import com.AWS.springboot.domain.posts.Posts;
 import com.AWS.springboot.domain.posts.PostsRepository;
+import com.AWS.springboot.web.dto.PostsListResponseDto;
 import com.AWS.springboot.web.dto.PostsResponseDto;
 import com.AWS.springboot.web.dto.PostsSaveRequestDto;
 import com.AWS.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -32,5 +36,12 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("게시글이 존재하지 않음. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
